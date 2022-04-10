5408f54d7f3475893e76db92eac5f6989ab97909
WissalMhm <102142630+WissalMhm@users.noreply.github.com>
2022-04-10 17:11:53 +0200

    Add files via upload

---
+++ patients-mvc/src/main/java/ma/enset/patientsmvc/PatientsMvcApplication.java
@@ -0,0 +1,35 @@
+package ma.enset.patientsmvc;
+
+import ma.enset.patientsmvc.entities.Patient;
+import ma.enset.patientsmvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientsMvcApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PatientsMvcApplication.class, args);
+    }
+  //  @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
+        return args -> {
+            patientRepository.save(
+                    new Patient(null,"Hassan",new Date(),false,122));
+            patientRepository.save(
+                    new Patient(null,"Mohammed",new Date(),true,321));
+            patientRepository.save(
+                    new Patient(null,"Yassmine",new Date(),true,165));
+            patientRepository.save(
+                    new Patient(null,"Hanane",new Date(),false,132));
+            patientRepository.findAll().forEach(p->{
+                System.out.println(p.getNom());
+            });
+        };
+    }
+}
